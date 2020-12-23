352f102611fd2ba3f58911d1dc236639a0152cdf
Jason Adam <jason.adam@aptiveresources.com>
2020-12-23 07:29:18 -0700

    updated load-env function

--- load-env.sh
+++ load-env.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Load Environment variable file for python virtual envs
-set -o allexport;
-source "$1";
-set +o allexport;
+load-env() {
+    set -o allexport;
+    source "$1";
+    set +o allexport;
+}
+
+load-env;
