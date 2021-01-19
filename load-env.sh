e1fd5eded6146766d0c262fb73bc03413fc074c6
jason-adam <22adamj22@gmail.com>
2021-01-19 11:36:36 -0700

    updated load-env function

--- load-env.sh
+++ load-env.sh
@@ -2,9 +2,9 @@
 
 # Load Environment variable file for python virtual envs
 load-env() {
-    set -o allexport;
-    source "$1";
-    set +o allexport;
+    set -o allexport \
+        && source "$1" \
+        && set +o allexport;
 }
 
 load-env $1;
