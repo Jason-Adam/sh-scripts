cedfc3f4bde2e46d3085bac34f63c3e9b9e50d12
jason-adam <22adamj22@gmail.com>
2021-02-12 09:09:15 -0700

    made scripts posix compliant

--- _install.sh
+++ _install.sh
@@ -7,13 +7,7 @@ mkdir -p "$HOME"/bin
 
 # Symlink all non install shell scripts to bin folder
 # Assumes you've cloned repo into ~/code
-<<<<<<< HEAD
 for f in scripts/*.sh; do
     fbname=$(basename "$f" | cut -d. -f1)
     ln -s -f "$(pwd)"/"$f" "$HOME"/bin/"$fbname"
-=======
-for file in *.sh; do
-    fbname=$(basename "$file" | cut -d. -f1)
-    ln -s -f "$HOME"/code/sh-scripts/"$file" "$HOME"/bin/"$fbname"
->>>>>>> 7a9821dc0fe5e3374acb205b6e9066ec8d1fe8d6
 done
